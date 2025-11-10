42c3ed1780be608c06c3a251d4c20c4f7f2ca82f
Sivakumar Balasubramanian <siva82kb@gmail.com>
2025-11-10 15:23:23 +0530

    first commit

---
+++ marsdefs.py
@@ -0,0 +1,95 @@
+"""
+Module containing definitions of different MARS related variables.
+
+Author: Sivakumar Balasubramanian
+Date: 17 October 2025
+Email: siva82kb@gmail.com
+"""
+
+from enum import Enum
+
+
+class PlutoEvents(Enum):
+    PRESSED = 0
+    RELEASED = 1
+    NEWDATA = 2
+
+LimbType = {
+    "NOLIMB":   0x00,
+    "RIGHT":    0x01,
+    "LEFT":     0x02,
+}
+
+ControlTypes = {
+    "NONE":             0x00,
+    "POSITION":         0x01,
+}
+
+OutDataType = {
+    "VERSION":      0x00,
+    "SENSORSTREAM": 0x01,
+    "CONTROLPARAM": 0x02,
+    "DIAGNOSTICS":  0x03
+}
+
+InDataType = {
+    "GET_VERSION":          0x00,
+    "RESET_PACKETNO":       0x01,
+    "SET_LIMB":             0x02,
+    "CALIBRATE":            0x03,
+    "START_STREAM":         0x04,
+    "STOP_STREAM":          0x05,
+    "SET_CONTROL_TYPE":     0x06,
+    "SET_CONTROL_TARGET":   0x07,
+    "SET_DIAGNOSTICS":      0x08,
+    "HEARTBEAT":            0x80,
+}
+
+ErrorTypes = {
+    "NOHEARTBEAT":          0x0000,
+    "ANG1MISMATCHERR":      0x0001,
+    "ANG234MISMATCHERR":    0x0002,
+    "ANG1JUMPERR":          0x0003,
+    "ANG234JUMPERR":        0x0004
+}
+
+MovementTypes = [
+    "MEDICAL-LATERAL",
+    "ANTERIOR-POSTERIOR",
+    "COMBINED"
+]
+
+OperationStatus = {
+    "NOERR":  0x00,
+    "YESERR": 0x01,
+}
+
+CommandStatus = {
+    "NONE":     0x00,
+    "SUCCESS":  0x01,
+    "FAIL":     0x02,
+}
+
+CalibrationStatus = {
+    "NOCALIB":  0x00,
+    "YESCALIB": 0x01,
+}
+
+MarsSensorDataNumber = {
+    "DUMMY":        0,
+    "SENSORSTREAM": 12,
+    "CONTROLPARAM": 8,
+    "DIAGNOSTICS":  17
+}
+
+INVALID_TARGET = 999.0
+CALIB_ANGLE_LIMIT = 50.0
+
+
+def get_name(def_dict: dict[str, int], code: int) -> str | None:
+    """Gets the name corresponding to the given code from the definition  dictionary.
+    """
+    for name, value in def_dict.items():
+        if value == code:
+            return name
+    return None
---
+++ qtjedi.py
@@ -0,0 +1,169 @@
+# Module implementing the JEDI serial communication protocol that work with 
+# the QT framework for emitting a signal when new data packets are available.
+#
+# Author: Sivakumar Balasubramanian
+# Date: 24 July 2024
+# Email: siva82kb@gmail.com
+
+
+import serial
+from serial import SerialException
+import enum
+import sys
+import time
+from PySide6.QtCore import (Signal, QThread)
+
+_INDEBUG = False
+_OUTDEBUG = False
+
+class JediParsingStates(enum.Enum):
+    LookingForHeader = 0
+    FoundHeader1 = 1
+    FoundHeader2 = 2
+    ReadingPayload = 3
+    CheckCheckSum = 4
+    FoundFullPacket = 5
+
+
+class JediComm(QThread):
+
+    newdata_signal = Signal(list)
+
+    def __init__(self, port: str | None = None, baudrate: int = 115200) -> None:
+        super().__init__()
+        self._port = port
+        self._baudrate = baudrate
+        self._ser = serial.Serial(port, baudrate)
+        self._state = JediParsingStates.LookingForHeader
+        self._in_payload = []
+        self._out_payload = []
+
+        # Payload reading variables.
+        self._n = 0
+        self._cnt = 0
+        self._chksum = 0
+
+        # thread related variables.
+        self._abort = False
+        self._pausing = False
+        # self.setDaemon(False)
+    
+    @property
+    def sleeping(self):
+        """ Returns if the thread is sleeping.
+        """
+        return self._pausing
+    
+    def is_open(self):
+        """Returns if the serial port is open.
+        """
+        return self._port if self._ser.is_open else ""
+
+    def send_message(self, outbytes: list[int]):
+        _outpayload = [0xAA, 0xAA, len(outbytes)+1, *outbytes]
+        _outpayload.append(sum(_outpayload) % 256)
+        # Send payload.
+        if _OUTDEBUG:
+            sys.stdout.write(f"\n [{time.time():6.3f}] [{len(_outpayload)}] Out data: ")
+            for _elem in _outpayload:
+                sys.stdout.write(f"{_elem} ")
+        self._ser.write(bytearray(_outpayload))
+
+    def run(self):
+        """
+        Thread operation.
+        """
+        self._state = JediParsingStates.LookingForHeader
+        while self._ser.is_open and not self._abort:
+            if self._pausing:
+                self.msleep(100)
+                continue
+            self._read_handle_data()
+            self.msleep(1)
+        try:
+            self._ser.close()  # safe to close here
+        except:
+            pass
+
+    def pause(self):
+        """
+        Puts the current thread in a paused state.
+        """
+        # with self.state:
+        self._pausing = True
+
+    def wakeup(self):
+        """
+        Wake up a paused thread.
+        """
+        self._pausing = False
+
+    def abort(self):
+        """
+        Aborts the current thread.
+        """
+        if self._pausing:
+            self.wakeup()
+        self._abort = True
+
+    def _read_handle_data(self):
+        """
+        Reads and handles the received data by calling the inform function.
+        """
+        # Read full packets.
+        if self._ser.in_waiting and _INDEBUG:
+            sys.stdout.write("\n New data: ")
+        try:            
+            while self._ser.in_waiting:
+                bytes_available = self._ser.in_waiting
+                if bytes_available > 0:
+                    _waiting_bytes = self._ser.read(bytes_available)
+                # _byte = self._ser.read()
+                print(_waiting_bytes)
+                for _byte in _waiting_bytes:
+                    if  _INDEBUG:
+                        sys.stdout.write(f"{_byte} ")
+                    if self._state == JediParsingStates.LookingForHeader:
+                        if _byte == 0xff:
+                            self._state = JediParsingStates.FoundHeader1
+                    elif self._state == JediParsingStates.FoundHeader1:
+                        if _byte == 0xff:
+                            self._state = JediParsingStates.FoundHeader2
+                        else:
+                            self._state = JediParsingStates.LookingForHeader
+                    elif self._state == JediParsingStates.FoundHeader2:
+                        # Payload size cannot be zero.
+                        if _byte == 0:
+                            self._state = JediParsingStates.LookingForHeader
+                            continue
+                        # Payload size is not zero.
+                        self._n = _byte
+                        self._cnt = 0
+                        self._chksum = 255 + 255 + self._n
+                        self._in_payload = [ 0 ] * (self._n - 1)
+                        self._state = JediParsingStates.ReadingPayload
+                    elif self._state == JediParsingStates.ReadingPayload:
+                        self._in_payload[self._cnt] = _byte
+                        self._chksum += _byte
+                        self._cnt += 1
+                        if self._cnt == self._n - 1:
+                            self._state = JediParsingStates.CheckCheckSum
+                    elif self._state == JediParsingStates.CheckCheckSum:
+                        if self._chksum % 256 == _byte:
+                            self._state = JediParsingStates.FoundFullPacket
+                        else:
+                            self._state = JediParsingStates.LookingForHeader
+                    
+                    # Handle full packet.
+                    if self._state == JediParsingStates.FoundFullPacket:
+                        print(self._in_payload[:12])
+                        self.newdata_signal.emit(self._in_payload)
+        except SerialException:
+            return
+
+
+if __name__ == '__main__':
+    jedireader = JediComm("COM5")
+    jedireader.start()
+    time.sleep(10)
+    jedireader.abort()
