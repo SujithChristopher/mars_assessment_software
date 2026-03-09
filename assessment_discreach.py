0d7cbeea39030fe7530bb8a8b81ce7e454848c94
SujithChristopher <chrisbon95@gmail.com>
2026-03-09 10:57:15 +0530

    still bigger

--- assessment_discreach.py
+++ assessment_discreach.py
@@ -35,7 +35,7 @@ class AssessmentDiscreteReachWindow(BaseAssessmentWindow):
     """Discrete Reaching assessment window."""
 
     # Constants
-    TARGET_SIZE = 0.06  # meters (6 cm)
+    TARGET_SIZE = 0.04  # meters (4 cm)
     TARGET_TOLERANCE = TARGET_SIZE * 0.5
     HOLD_TIME = 3.0  # seconds
 
